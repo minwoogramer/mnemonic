2c0a5d88c997e46da2384df60757d285dc1ad698
namminwoo-hub <100527953+minwoogramer@users.noreply.github.com>
2022-06-10 19:39:08 +0900

    mnmonic

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', function(req, res, next) {
 });
 app.use('/wallet', walletRouter);
 
-// catch 404 and forward to error handler
+// 404 error catch
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err['status'] = 404;
